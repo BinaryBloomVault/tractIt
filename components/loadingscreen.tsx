bf343ebf6db8b1a4349ec6f921b03f987e98c867
Kirvy D <dianon.kirvy@gmail.com>
2024-04-15 21:52:29 +0800

    add components

---
+++ components/loadingscreen.tsx
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const LoadingScreen = () => {
+  return <Text>Loading...</Text>;
+};
+
+export default LoadingScreen;
